b5accd5fea08d567c2ef0ca5d602b7101ff6e96f
Paula Spitale <35274131+paulaspi@users.noreply.github.com>
2018-03-03 16:14:25 -0600

    storm center
    Storm center form added.
    Javascript for slider added.
    Thank you page created.

---
+++ assignments/franklin-8/scripts/adjustrating.js
@@ -0,0 +1,12 @@
+/*var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+
+output.innerHTML = slider.value;
+
+slider.oninput = funtion() {
+    output.innerHTML = this.value;
+}*/
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
