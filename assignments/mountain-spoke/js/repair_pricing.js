b52a63350e5909cb61fab671e8153dafa2c91c44
Paula Spitale <35274131+paulaspi@users.noreply.github.com>
2018-04-09 23:40:57 -0500

    json part 5

--- assignments/mountain-spoke/js/repair_pricing.js
+++ assignments/mountain-spoke/js/repair_pricing.js
@@ -1,12 +1,12 @@
 $.getJSON('data/pricing.json', function(data){
-    console.log(data);
+    console.log(prices);
 
-document.getElementById('tire').innerHTML = data.prices[0];
-document.getElementById('brake').innerHTML = data.prices[1];
-document.getElementById('spoke').innerHTML = data.prices[2];
-document.getElementById('shift').innerHTML = data.prices[3];
-document.getElementById('gold').innerHTML = data.prices[4];
-document.getElementById('platinum').innerHTML = data.prices[5];
+document.getElementById('tire').innerHTML = prices.tire;
+document.getElementById('brake').innerHTML = prices.brake;
+document.getElementById('spoke').innerHTML = prices.spoke;
+document.getElementById('shift').innerHTML = prices.shift;
+document.getElementById('gold').innerHTML = prices.gold;
+document.getElementById('platinum').innerHTML = prices.platinum;
  
 });
 
