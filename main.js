16ccd0ea12817a7610d255377be093f4944a1ca8
Paula Spitale <paulaspi@hotmail.com>
2022-09-14 16:18:53 -0500

    story editor exercise

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+/* function to add message below a button. Change text of button once clicked
 function showCongratulations(theButtonClicked){
     document.getElementById("result_display").innerHTML = 
         "Congratulations, you did it! You got the page to update!"
     theButtonClicked.innerHTML = "Click Me Again!"
-}
+}*/
