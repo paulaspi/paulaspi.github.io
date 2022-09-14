cf6bb7b12d2fdb09432b280a173d04825e542ca7
Paula Spitale <paulaspi@hotmail.com>
2022-09-14 17:17:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ function showCongratulations(theButtonClicked){
         "Congratulations, you did it! You got the page to update!"
     theButtonClicked.innerHTML = "Click Me Again!"
 }*/
+
+function loadStory(){
+    var storyName = document.getElementById('name_input').value 
+    var storyHTML = localStorage.getItem(storyName)
+    document.getElementById("story_editor").value = storyHTML
+}
+
+function saveStory(){
+    var storyName = document.getElementById("name_input").value
+    var storyHTML = document.getElementById("story_editor").value
+    localStorage.setItem(storyName, storyHTML)
+}
+
+function displayStory(){
+    var storyHTML = document.getElementById("story_editor").value
+    document.getElementById("story_display").innerHTML = storyHTML
+}
