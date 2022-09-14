45bd38404f0e1809f8296c45d6111b140901e050
Paula Spitale <paulaspi@hotmail.com>
2022-09-14 14:28:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function showCongratulations(theButtonClicked){
     document.getElementById("result_display").innerHTML = 
         "Congratulations, you did it! You got the page to update!"
+    theButtonClicked.innerHTML = "Click Me Again!"
 }
